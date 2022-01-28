2a72dab7460c5a236feeb2f9de7dcfc783743279
L-tech <legendabrahamonoja@gmail.com>
2022-01-28 03:14:28 +0100

    update: unpack NFT using fronted

--- pages/lounge.tsx
+++ pages/lounge.tsx
@@ -1,11 +1,11 @@
 import { useWeb3 } from "@3rdweb/hooks";
 import { ThirdwebSDK } from "@3rdweb/sdk";
-import type { PackMetadataWithBalance } from "@3rdweb/sdk";
+import type { PackMetadataWithBalance, BundleMetadata } from "@3rdweb/sdk";
 import { useEffect, useState } from "react";
-import { packAddress } from "../lib/contractAddresses";
 import NFT from "../components/nft";
 import OpenButton from "../components/open-button";
-const { address, provider } = useWeb3();
+import { bundleAddress, packAddress } from "../lib/contractAddresses";
+const { address, provider } = useWeb3()
 const signer = provider?.getSigner()
 
 
@@ -19,18 +19,26 @@ export function getStaticProps() {
 }
 
 export default function Lounge() {
-  const { address } = useWeb3()
+  const { address, provider } = useWeb3();
   const [loading, setLoading] = useState(false);
   const [packNfts, setPackNfts] = useState<PackMetadataWithBalance[]>([]);
+  const [bundleNfts, setBundleNfts] = useState<BundleMetadata[]>([])
 
-  const sdk = new ThirdwebSDK("https://rpc-mumbai.maticvigil.com")
+  const sdk = new ThirdwebSDK("https://rpc-mumbai.maticvigil.com");
   const packModule = sdk.getPackModule(packAddress);
+  const bundleModule = sdk.getBundleModule(bundleAddress);
 
   async function getNfts() {
-    const fetchedPackNfts = await packModule.getOwned(address);
-    console.log(fetchedPackNfts);
+    const [fetchedPackNfts, fetchedBundleNfts] = await Promise.all([
+      packModule.getOwned(address),
+      bundleModule.getOwned(address),
+    ])
+    console.log({ fetchedPackNfts, fetchedBundleNfts });
     setPackNfts(fetchedPackNfts);
+    setBundleNfts(fetchedBundleNfts);
   }
+  
+  
   
   async function getNftsWithLoading() {
     setLoading(true);
@@ -67,7 +75,7 @@ export default function Lounge() {
     )
   }
   
-  if (packNfts.length === 0) {
+  if (packNfts.length === 0 && bundleNfts.length === 0) {
     return (
       <p>You need to own some NFTs to access the lounge!</p>
     )
@@ -87,7 +95,19 @@ export default function Lounge() {
           </div>
         </div>
       )}
-  
+      {bundleNfts.length > 0 && (
+      <div>
+        <h2 className="text-4xl font-bold">Your Collection</h2>
+        <div className="grid grid-cols-2 md:grid-cols-3 mt-4 gap-2">
+          {bundleNfts.map((nft) => (
+            <div className="border border-blue-500 rounded-lg p-4" key={nft.metadata.id}>
+              <NFT metadata={nft.metadata} />
+              <p className="text-gray-800">Balance: {nft.ownedByAddress.toString()}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    )}
       <div>
         <h2 className="text-4xl font-bold">Some secret content!</h2>
         <p>This content is only available to users with NFTs! 🤫</p>
